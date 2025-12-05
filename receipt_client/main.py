e3d4c3fec251a93af0cc63f6eceda40a9c6affef
Michael Canudas <michael@canudas.dev>
2025-12-05 13:06:34 -0500

    receipt client

---
+++ receipt_client/main.py
@@ -0,0 +1,36 @@
+import socket
+from escpos.printer import Usb
+
+def print_message(printer, message):
+    printer.text(message)
+    print("Printed message {message}")
+
+def receive_from_server(conn):
+    printer = Usb(0x0416, 0x5011, in_ep=0x81, out_ep=0x03, profile="POS-5890")
+    try:
+        while True:
+            raw = conn.recv(1024).decode()
+            if not raw:
+                break
+
+            action, param = raw.split(':', 1)
+            if action == "print":
+                print_message(printer, param)            
+            else:
+                conn.send(b'Action does not exist')
+                continue
+
+            conn.send(b'Action handled')
+    finally:
+        conn.close()
+
+def start_client(host='127.0.0.1', port=7531):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        print("Listening...")
+
+        s.send('printer'.encode())
+        receive_from_server(s)
+
+
+start_client()
