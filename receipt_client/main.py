c0c8c9bc06bc3c360c5cff98d497054be5fc775c
Michael Canudas <michael@canudas.dev>
2025-12-05 14:31:43 -0500

    update receipt

--- receipt_client/main.py
+++ receipt_client/main.py
@@ -4,9 +4,9 @@ from escpos.printer import Usb
 HOST = '10.66.236.77'
 PORT = 7531
 
-def print_message(printer, message):
-    printer.text(message.replace('\\n', '\n'))
-    print(f"Printed message {message}")
+def print_message(printer, args):
+    printer.text(args.replace('\\n', '\n'))
+    print(f"> Printed: {args}")
 
 def receive_from_server(conn):
     printer = Usb(0x0416, 0x5011, in_ep=0x81, out_ep=0x03, profile="POS-5890")
@@ -16,21 +16,17 @@ def receive_from_server(conn):
             if not raw:
                 break
 
-            action, param = raw.split(':', 1)
-            if action == "print":
-                print_message(printer, param)            
-            else:
-                conn.send(b'Action does not exist')
-                continue
+            command, args = raw.split(':', 1)
+            if command == "print":
+                print_message(printer, args)     
 
-            conn.send(b'Action handled')
     finally:
         conn.close()
 
 def start_client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        print("Listening...")
+        print("> Listening...")
 
         s.send('printer'.encode())
         receive_from_server(s)
